b1f6c7ea579c0f8215d76aed1e85c0685a259e9a
Dhananjay Mane <dhananjay.mane@mobisoftinfotech.com>
2019-09-30 19:35:59 +0530

    Added db field changes

--- todo/models.py
+++ todo/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-      title = models.CharField(max_length=120)
-      description = models.TextField()
-      completed = models.BooleanField(default=False)
+      name = models.CharField(max_length=30)
+      age = models.CharField(max_length=10)
 
       def _str_(self):
-        return self.title
+        return self.name
